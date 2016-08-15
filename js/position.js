be977db1555edf4e71ad1506d60d598f89b209fb
AnkurSarode <ankur.sarode@gmail.com>
2016-08-15 16:52:59 +0530

    Some changes

--- js/position.js
+++ js/position.js
@@ -14,8 +14,29 @@ $(document).ready(function()
     $('.concentric-circle-holder').css('top',-1*gola_holder_ht/1.9);
   }
 
+
+  $(document).scroll(function()
+  {
+    var scroll=$(window).scrollTop();
+    if(scroll>=Ht)
+    {
+      $('#philosophy-row .landing-logo-holder').css('position','fixed');
+      $('#philosophy-row .landing-logo-holder').css('top','3px');
+    }
+    else
+    {
+      $('#philosophy-row .landing-logo-holder').css('position', 'absolute');
+      $('#philosophy-row .landing-logo-holder').css('top','auto');
+    }
+  });
+
+});
+
+
   $(window).resize(function()
   {
+    var Ht=$(window).height();
+    var Wt=$(window).width();
     var gola_holder_ht=$('.concentric-circle-holder').height();
     if(Wt<600)
     {
@@ -26,5 +47,3 @@ $(document).ready(function()
       $('.concentric-circle-holder').css('top',-1*gola_holder_ht/1.9);
     }
   });
-
-});
